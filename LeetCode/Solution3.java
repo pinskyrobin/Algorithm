3681aa0439fc76b05bcd1742a14ccb0b81639f32
PinskyRobin <xu568059888@163.com>
2022-02-09 10:25:13 +0800

    滑动窗口，原先的代码不能通过，现已修复

--- LeetCode/Solution3.java
+++ LeetCode/Solution3.java
@@ -1,25 +1,49 @@
 import java.util.*;
 
+/**
+ * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+ */
 public class Solution3 {
+    // 初始方案
+    // public int lengthOfLongestSubstring(String s) {
+    //     if (s.length() < 2)    return s.length();
+
+    //     int ans = 0;
+    //     int start = 0;
+    //     int cur = 0;
+    //     char[] str = s.toCharArray();
+    //     List<Character> charList = new ArrayList<>();
+    //     for (cur = 0; cur < str.length; cur++) {
+    //         if (!charList.contains(str[cur])) {
+    //             charList.add(str[cur]);
+    //         } else {
+    //             ans = Math.max((cur - start), ans);
+    //             while (charList.contains(str[cur])) {
+    //                 charList.remove(0);
+    //                 start++;
+    //             }
+    //             charList.add(str[cur]);
+    //         }
+    //     }
+    //     return Math.max((cur - start), ans);
+    // }
+    // 滑动窗口
     public int lengthOfLongestSubstring(String s) {
-        if (s.length() == 1) {
-            return 1;
-        }
-        char[] c = s.toCharArray();
-        int max = 0;
-        List<Character> list = new ArrayList<>(c.length);
-        int begin = 0;
-        for (int end = 0; end < s.length(); end++) {
-            if (!list.contains(c[end])) {
-                list.add(c[end]);
-            } else {
-                max = ((end - begin) > max) ? (end - begin) : max;
-                while (list.contains(c[end])) {
-                    list.set(begin++, ' ');
-                }
-                list.add(c[end]);
+        int start = 0, cur = 0;
+        int ans = 0;
+        char[] str = s.toCharArray();
+        Set<Character> set = new HashSet<>(str.length);
+
+        for (; cur < str.length; cur++) {
+            if (set.contains(str[cur])) {
+                ans = Math.max((cur - start), ans);
+                while (set.contains(str[cur]))
+                    set.remove(str[start++]);
             }
+            set.add(str[cur]);
         }
-        return max;
+
+        return Math.max((cur - start), ans);
+
     }
 }
